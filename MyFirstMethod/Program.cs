16f77803d08e7b71f62ba8680887ed8df69a8434
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-03-08 15:43:14 +0000

    make file

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -8,8 +8,15 @@ namespace MyFirstMethod
 {
     class Program
     {
+        static void CountTo10()
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+        }
         static void Main(string[] args)
         {
+            CountTo10();
+            CountTo10();
         }
     }
 }
